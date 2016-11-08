c7ec0643821fa7d135e00709753a9f782644da5d
shiqiyue <835802084@qq.com>
2016-11-08 23:44:42 +0800

    init

---
+++ log/log4j/src/main/java/log4j/App.java
@@ -0,0 +1,16 @@
+package log4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	static Logger logger = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		logger.info("wwwww,{}{}","dad","aa");
+	}
+}
---
+++ log/logback/src/main/java/logback/App.java
@@ -0,0 +1,18 @@
+package logback;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	static Logger logger = LoggerFactory.getLogger(App.class);
+    public static void main( String[] args )
+    {
+    	logger.info("who am i {} , {}", "wuwen1ya", "222");
+    }
+}
